fc33716b373bb6c1a6c4ff0cf5ef4d09701e039f
sturdy-robot <pedrenriquegg@hotmail.com>
2021-12-16 16:28:16 -0300

    add csv_columns exercise

---
+++ csv_columns/csv_columns.py
@@ -0,0 +1,19 @@
+from itertools import zip_longest
+import csv
+
+
+def csv_columns(file, headers=None, missing=None):
+    reader = csv.reader(file)
+    dictionary = {}
+    columns = list(zip_longest(*reader, fillvalue=missing))
+    if headers is None:
+        headers = [col[0] for col in columns]
+
+    for column, header in zip_longest(columns, headers):
+        col = []
+        for item in column:
+            if item != header:
+                col.append(item)
+        dictionary[header] = col
+    
+    return dictionary
