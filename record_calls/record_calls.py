0891632bd3b6634346e3105667ce0ab26b50d091
sturdy-robot <pedrenriquegg@hotmail.com>
2021-12-27 14:41:10 -0300

    improve record_calls exercise

--- record_calls/record_calls.py
+++ record_calls/record_calls.py
@@ -1,11 +1,10 @@
 from functools import wraps
 
 
-class record_calls:
-    def __init__(self, func):
-        self.func = func
-        self.call_count = 0
-
-    def __call__(self, *args, **kwargs):
-        self.call_count += 1
-        return self.func(*args, **kwargs)
+def record_calls(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.call_count += 1
+        return func(*args, **kwargs)
+    wrapper.call_count = 0
+    return wrapper
