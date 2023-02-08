4e8161d3ef5720ee9559251a8d8e001fe864306d
sturdy-robot <pedrenriquegg@hotmail.com>
2023-02-08 18:05:50 -0300

    update: minmax2

--- minmax2/minmax2.py
+++ minmax2/minmax2.py
@@ -1,16 +1,22 @@
+from itertools import tee
+from typing import NamedTuple, Any
 DEFAULT_VALUE = object()
 
 
-def minmax2(lst, *, key=None, default=DEFAULT_VALUE):
-    if not lst:
+def minmax2(*lst, key=None, default=DEFAULT_VALUE):
+    l = iter(*lst)
+    l, l2, l3 = tee(l, 3)
+
+    try:
+        next(l3)
+    except StopIteration:
         if default == DEFAULT_VALUE:
             raise ValueError
         return default, default
 
-    if not isinstance(lst, list):
-        lst = iter(lst)
+    return MinMax(min(l, default=default, key=key), max(l2, default=default, key=key))
 
-    if key is None:
-        return min(lst), max(lst)
-    else:
-        return min(lst, key=key), max(lst, key=key)
+
+class MinMax(NamedTuple):
+    min: Any
+    max: Any
